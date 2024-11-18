22cd439b925cd0ec8105efe2527fd2accc38cb5d
rpghost <rishabpillai78@gmail.com>
2024-11-18 16:53:47 +0530

    made the navbar with the routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Contact from './Contact';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='bg-indigo-900'>
+  <Routes>
+    <Route path='/' element={<Navbar></Navbar>}>
+    <Route path="home" element={<Home />} />
+    <Route path="contact" element={<Contact />} />
+    <Route path="log-in" element={<Login />} />
+    </Route>
+  </Routes>
+  </div>
+  </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <>
+            <div className='bg-violet-800 flex flex-row justify-between'>
+
+                <div className='flex flex-row'>
+                    <div className=' relative text-white text-4xl m-4 font-permanent-marker text-emerald-300'>
+                        <Link to="/home">
+                            StayHungri</Link></div>
+                    <div className=' relative text-white text-xl m-7 hover:text-violet-200 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-1 after:bg-purple-400 after:transition-all after:ease-in-out after:duration-300 hover:after:w-full '>
+                        <Link to="/home">
+                            Home</Link></div>
+                    <div className=' relative text-white text-xl m-7 hover:text-violet-200 after:absolute after:left-0 after:bottom-0 after:w-0 after:h-1 after:bg-purple-400 after:transition-all after:ease-in-out after:duration-300 hover:after:w-full '>
+                        <Link to="/contact">
+                            Contact</Link></div>
+                </div>
+                <div className=' relative text-white text-xl m-7 hover:text-violet-200 '>
+                    <Link to="/log-in">
+                        Log in</Link></div>
+            </div>
+            <Outlet/>
+        </>
+    )
+}
+
+
+// below is the css code to create a custom underline
+
+// .button::after {
+//   content: '';
+//   position: absolute;
+//   bottom: 0;
+//   left: 0;
+//   width: 0%;
+//   height: 2px;
+//   background-color: red;
+//   transition: width 0.3s;
+// }
+
+// .button:hover::after {
+//   width: 100%;
+// }
