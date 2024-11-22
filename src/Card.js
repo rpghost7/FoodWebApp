48385889db699763243ab143ccff82267fd4093d
rpghost <rishabpillai78@gmail.com>
2024-11-22 22:59:31 +0530

    just made it into a responsive grid

--- src/Card.js
+++ src/Card.js
@@ -1,78 +1,41 @@
-import React, {  useState } from 'react'
-import chicken from './Chicken-Biryani-Recipe.jpg'
+import React, { useState } from 'react';
+import chicken from './Chicken-Biryani-Recipe.jpg';
 import { ReactComponent as StarIcon } from './Star.svg';
 
 export default function Card() {
   const [rating, setRating] = useState(0);
   const percentage = Math.round((4 / 5) * 100);
-  function handleRating(){
+
+  function handleRating() {
     setRating(4);
   }
+
   return (
-    <>
-      <div className='flex flex-row h-auto w-full mt-32 px-12 pb-10 '>
-        <div className='w-full h-full rounded-md '>
-          <div className='bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12'>
-            <img className='w-full object-fill rounded-md' src={chicken} alt='chickenBiryani' />
-            <p className='text-white text-2xl p-2'>Card title</p>
-            <p className='text-white p-2 inline-flex relative'>Rating:{handleRating}
-            
-              {Array.from(Array(5).keys()).map((_, i) => (
-                <StarIcon key={i} className="mr-0.5 w-5 h-5 flex z-10 translate-y-0.5" style={{color:'#ff8f00'}} />
-
-              ))}
-               <div className="bg-violet-600 absolute top-0 bottom-0 right-0  mix-blend-color z-20" style={{ width: `${100 - percentage}%` }} />
-            </p>
-            <p className='text-white p-2'>Expected time:</p>
+    <div className='mt-32 px-12 pb-10'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16'>
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div key={index} className='w-full h-full rounded-md'>
+            <div className='bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12'>
+              <img className='w-full object-fill rounded-md' src={chicken} alt='chickenBiryani' />
+              <p className='text-white text-2xl p-2'>Card title</p>
+              <p className='text-white p-2 inline-flex relative'>
+                Rating: {handleRating}
+                {Array.from(Array(5).keys()).map((_, i) => (
+                  <StarIcon key={i} className="mr-0.5 w-5 h-5 flex z-10 translate-y-0.5" style={{ color: '#ff8f00' }} />
+                ))}
+                <div className="bg-violet-600 absolute top-0 bottom-0 right-0 mix-blend-color z-20" style={{ width: `${100 - percentage}%` }} />
+              </p>
+              <p className='text-white p-2'>Expected time:</p>
+            </div>
           </div>
-        </div>
-        <div className='w-full h-full rounded-md mx-20'>
-          <div className='bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12'>
-            <img className='w-full object-fill rounded-md' src={chicken} alt='chickenBiryani' />
-            <p className='text-white text-2xl p-2'>Card title</p>
-            <p className='text-white p-2 inline-flex relative'>Rating:{handleRating}
-              {Array.from(Array(5).keys()).map((_, i) => (
-                <StarIcon key={i} className="mr-0 w-5 h-5 flex z-10  translate-y-0.5" style={{color:'#ff8f00'}} />
-
-              ))}
-               <div className="bg-violet-600 absolute top-0 bottom-0 right-0  mix-blend-color z-20" style={{ width: `${100 - percentage}%` }} />
-            </p>
-            <p className='text-white p-2'>Expected time:</p>
-          </div>
-        </div>
-        <div className='w-full h-full rounded-md '>
-          <div className='bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12'>
-            <img className='w-full object-fill rounded-md' src={chicken} alt='chickenBiryani' />
-            <p className='text-white text-2xl p-2'>Card title</p>
-            <p className='text-white p-2 inline-flex relative'>Rating:{handleRating}
-              {Array.from(Array(5).keys()).map((_, i) => (
-                <StarIcon key={i} className="mr-0.5 w-5 h-5 flex z-10 translate-y-0.5" style={{color:'#ff8f00'}} />
-
-              ))}
-               <div className="bg-violet-600 absolute top-0 bottom-0 right-0  mix-blend-color z-20" style={{ width: `${100 - percentage}%` }} />
-            </p>
-            <p className='text-white p-2'>Expected time:</p>
-          </div>
-        </div>
-        <div className='w-full h-full rounded-md ml-20'>
-          <div className='bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12'>
-            <img className='w-full object-fill rounded-md' src={chicken} alt='chickenBiryani' />
-            <p className='text-white text-2xl p-2'>Card title</p>
-            <p className='text-white p-2 inline-flex relative'>Rating:{handleRating}
-              {Array.from(Array(5).keys()).map((_, i) => (
-                <StarIcon key={i} className="mr-0.5 w-5 h-5 flex z-10 translate-y-0.5" style={{color:'#ff8f00'}} />
-
-              ))}
-               <div className="bg-violet-600 absolute top-0 bottom-0 right-0  mix-blend-color z-20" style={{ width: `${100 - percentage}%` }} />
-            </p>
-            <p className='text-white p-2'>Expected time:</p>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 // https://samuelkraft.com/blog/fractional-svg-stars-css
 // the above link was used to create the star rating system and it's css
 // thanks to that it is working
+// also use gap when making space between rows and columns while doing it in a grid
+// or flex box
