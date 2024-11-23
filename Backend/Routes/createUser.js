8879bee296ccdcffa9396f593c4425704d653ea6
rpghost <rishabpillai78@gmail.com>
2024-11-23 22:46:52 +0530

    created some constraints while giving username,name etc

--- Backend/Routes/createUser.js
+++ Backend/Routes/createUser.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../modules/User');
+const { body, validationResult } = require('express-validator');
+// this thing is called express validator 
 // we are taking take collection from the file
-router.post('/createuser', async (req, res) => {
+router.post('/createuser',[
+ body('name').isLength({ min: 3 }),
+//  checks whether the name is at least 3 characters long
+ body('email').isLength({min:5}),
+ body('password','Your password is too short').isLength({min:8})
+//  checks whether the password is at least 8 characters longs
+],
+     async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        // this gives an error if the above conditions don't match or break
     try {
+
         await User.create({
             name: req.body.name,
             email:req.body.email,
