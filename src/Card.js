e512f955d6a874d4d6832f683ca6ffcafb12e523
rpghost <rishabpillai78@gmail.com>
2024-12-13 15:36:08 +0530

    added search bar function

--- src/Card.js
+++ src/Card.js
@@ -15,18 +15,25 @@ import { ReactComponent as StarIcon } from './Star.svg';
 
 
 export default function Card() {
- 
-  const [data, setData] = useState({});
-  const [hoverItemId,setHoverItemId]=useState(null);
 
-function handleMouseEnter(Id){
-  setHoverItemId(Id);
-}
-function handleMouseLeave(){
-  setHoverItemId(null);
-}
+  const [data, setData] = useState({});
+  const [hoverItemId, setHoverItemId] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+
+  };
+  function handleMouseEnter(Id) {
+    setHoverItemId(Id);
+  }
+  function handleMouseLeave() {
+    setHoverItemId(null);
+  }
   // still trying to figure out a way to give custom rating according to the products
-  
+  const filteredItems = data.food_items?.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    // includes is used to find out which of the dishes or strings contains the letters we are typing
+  );
   const images = {
     "Chicken Biryani": chickenBiryani,
     "Paneer 65": paneer65,
@@ -67,34 +74,50 @@ function handleMouseLeave(){
 
   return (
     <div className="mt-32 px-12 pb-10 text-white">
-      {data.food_category?.map(item => (
-        <div key={item.CategoryName}>
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          className="p-2 border rounded text-black w-full bg-violet-400 placeholder:text-stone-800"
+        />
+      </div>
+      {/* here ?  is used because react state updates before the api calls which means that data is undefined or null initially and after the api call it gets defined
+      so we use optional chaining i.e the question mark to avoid the undefined part and allow rendering when the data is available */}
+      {data.food_category?.map(item => {
+         const categoryItems = filteredItems.filter(
+          (datas) => datas.CategoryName === item.CategoryName
+        );
+        if (categoryItems.length === 0) return null;
+
+        return(<div key={item.CategoryName}>
           <h2 className="text-3xl font-bold my-8">{item.CategoryName}</h2>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-16">
-            {data.food_items
+            {filteredItems
               .filter(datas => datas.CategoryName === item.CategoryName)
               .map(foodItem => {
                 // Calculate the percentage for the current item's rating
-               const no = parseFloat(foodItem.rating)
+                const no = parseFloat(foodItem.rating)
                 const percentage = Math.round((no / 5) * 100);
 
                 return (
-                  <div key={foodItem._id} className="w-5/6 h-full rounded-md">
-                    <div className="bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12" onMouseEnter={()=>{
+                  <div key={foodItem._id} className="w-11/12 h-full rounded-md">
+                    <div className="bg-violet-600 rounded-md hover:w-11/12 hover:h-11/12" onMouseEnter={() => {
                       handleMouseEnter(foodItem._id);
-                    }} onMouseLeave={()=>{
+                    }} onMouseLeave={() => {
                       handleMouseLeave();
                     }}>
-                    
+
                       <img
-                        className ={` w-full h-64 object-fill rounded-md ${hoverItemId === foodItem._id? 'recti' : ' '}`}
+                        className={` w-full h-64 object-fill rounded-md ${hoverItemId === foodItem._id ? 'recti' : ' '}`}
                         src={images[foodItem.name]}
                         alt={foodItem.name}
                       />
-                      
+
                       <p className="text-white text-2xl p-2">{foodItem.name}</p>
                       <p className="text-white p-2 inline-flex relative">
-                        Rating : 
+                        Rating :
                         {Array.from(Array(5).keys()).map((_, i) => (
                           <StarIcon
                             key={i}
@@ -116,7 +139,7 @@ function handleMouseLeave(){
 
 
         </div>
-      ))}
+      )})}
     </div>
   );
 }
