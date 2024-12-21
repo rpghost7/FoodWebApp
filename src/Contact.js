3f16c7ac207d03f5eb1782d890bd11cae077c51a
rishabh <rishabpillai78@gmail.com>
2024-12-22 01:18:07 +0530

    just added a description of my work in the contact page

--- src/Contact.js
+++ src/Contact.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className='text-white'>
-      this is the contact page
+    <div className='flex justify-center'>
+    <div className='text-white text-center text-lg my-6 w-2/5'>
+      Hello I am Rishabh Pillai, and this is my food delivery website project I made using MERN stack. I used twilio api for the otp function, and hopefully i am able to use stripe to create a payment method through which people can actually checkout, that way the app is now truly complete. If there are any bugs or issues or that I should add more features, please let me know. I am open to any feedback and suggestions. Thank you.
+    </div>
     </div>
   )
 }
